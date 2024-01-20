efda434c0271c20a518a3cfa6d531c8f17693fa6
PrasadPonnada <PrasadPonnada09@gmail.com>
2024-01-20 13:48:34 +0530

    mobile theme changes

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -5,9 +5,9 @@ import {
 } from "@/components/ui/sheet"
 import Image from "next/image";
 import MenuIcon from '@/public/assets/icons/menu.svg'
-import Icon from '@/public/assets/images/logo.svg'
 import { Separator } from "../ui/separator";
 import NavItems from "./NavItems";
+import AppIcon from "./AppIcon";
 
 
 const MobileNav = () => {
@@ -17,10 +17,10 @@ const MobileNav = () => {
                 <SheetTrigger>
                     <Image
                         src={MenuIcon} alt="menu" width={24}
-                        height={24} className="cursor-pointer" />
+                        height={24} className="cursor-pointer dark:filter-grey " />
                 </SheetTrigger>
-                <SheetContent className="flex flex-col bg-white gap-6 md:hidden">
-                    <Image src={Icon} alt="icon" width={128} height={38} />
+                <SheetContent className="flex flex-col bg-white  dark:bg-popover  gap-6 md:hidden">
+                    <AppIcon />
                     <Separator className="border-gray-50 border" />
                     <NavItems />
                 </SheetContent>
